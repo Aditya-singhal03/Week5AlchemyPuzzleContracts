78af005bda3e10ff7431235a2dc57ca20f0f04c2
Adityasinghal03 <singhalbspr@gmail.com>
2023-07-06 16:26:01 +0530

    first commit

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,19 +1,41 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
+const { ethers } = require('hardhat');
 
 describe('Game5', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
 
-    return { game };
+    const threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
+
+    let  valid,wallet,address;
+
+    while(!valid){
+      wallet = ethers.Wallet.createRandom();
+      address = await wallet.getAddress();
+
+      if(address<threshold){
+        valid=true;
+        wallet=wallet.connect(ethers.provider);
+      }
+    }
+
+    const signer = await ethers.provider.getSigner(0);
+
+    await signer.sendTransaction({
+        to: address,
+        value: ethers.utils.parseEther('0.1')
+      })
+
+    return { game , wallet};
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game,wallet } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
 
-    await game.win();
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
